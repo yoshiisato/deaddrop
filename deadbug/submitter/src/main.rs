2581e1f2b3d529fda1322b0135f7efeb8c184b8b
Harjasleen Malvai <harjasleen_malvai@brown.edu>
2025-06-13 17:27:25 -0400

    Added some content to main in submitter

--- deadbug/submitter/src/main.rs
+++ deadbug/submitter/src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 4 {
+        eprintln!("Usage: {} <bug_file> <enc_pk> <clue_key>", args[0]);
+        std::process::exit(1);
+    }
+
+    let bug_file = &args[1];
+    let enc_pk = &args[2];
+    let clue_key = &args[3];
+
+    println!("Bug file: {}", bug_file);
+    println!("Enc PK: {}", enc_pk);
+    println!("Clue key: {}", clue_key);
+
+    let mut bug_file = File::open(bug_file).unwrap();
+    let mut buffer = Vec::new();
+    bug_file.read_to_end(&mut buffer).unwrap();
+    let bug = buffer.as_slice();
+
+    // Rest is TODO right now
+
 }
