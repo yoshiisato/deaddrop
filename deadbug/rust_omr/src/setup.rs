643a7655845c19758a15f8a2b7ecc364730548f4
Harjasleen Malvai <harjasleen_malvai@brown.edu>
2025-06-14 17:41:54 -0400

    Randomized omr keys

--- deadbug/rust_omr/src/setup.rs
+++ deadbug/rust_omr/src/setup.rs
@@ -8,14 +8,23 @@ pub fn gen_param(lambda: usize, epsilon_p: f64, epsilon_n: f64) -> PublicParams 
     }
 }
 
+fn generate_random_32_bytes() -> [u8; 32] {
+    let mut bytes = [0u8; 32];
+    OsRng.fill_bytes(&mut bytes);
+    bytes
+}
+
 pub fn keygen(_pp: &PublicParams) -> (SecretKey, PublicKey) {
     // For now, use random byte vectors as keys
     let sk = SecretKey {
-        sk_bytes: vec![0; 32],
+        sk_bytes: generate_random_32_bytes().to_vec(),
     };
+    let random_bytes = generate_random_32_bytes();
+    // For simplicity, use the same random bytes for both public keys
+    
     let pk = PublicKey {
-        pk_clue: vec![1; 32],
-        pk_detect: vec![1; 32],
+        pk_clue: random_bytes.to_vec(),
+        pk_detect: random_bytes.to_vec(),
     };
     (sk, pk)
 }
