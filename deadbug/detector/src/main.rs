bed776c7aa261a8aeab971fa1f28e5b1a58491dd
marwamou <marwamouallem@gmail.com>
2025-06-15 00:49:02 +0300

    detector code

--- deadbug/detector/src/main.rs
+++ deadbug/detector/src/main.rs
@@ -1,3 +1,29 @@
+use deadbug_detector::detector::Detector;
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Begin detection!");
+
+    let args: Vec<String> = env::args().collect();
+
+    // If you expect exactly one argument (besides program name):
+    if args.len() != 3 {
+        eprintln!("Usage: {} <your_input>", args[0]);
+        std::process::exit(1);
+    }
+
+    let input = &args[1];
+    let path: &str = &args[2];
+    println!("pk_detector: {}", input);
+    println!("path for bb: {}", path);
+
+
+    let k_bound = 10;
+    let detector = Detector::new(input.as_bytes(),path, k_bound);
+    println!("Detection complete!");
+    detector.get_payloads();
+    println!("Payloads: {:?}", detector.get_payloads());
+    
 }
+
