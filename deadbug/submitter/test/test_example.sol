39008025abde2f9249a4bf7f046046feb36a245f
Harjasleen Malvai <harjasleen_malvai@brown.edu>
2025-06-15 12:09:29 -0400

    Added comments to bug and test file

--- deadbug/submitter/test/test_example.sol
+++ deadbug/submitter/test/test_example.sol
@@ -20,11 +20,15 @@ contract DAOTest is Test, BugReport {
         forkId = vm.createFork(rpcUrl, vm.envUint("block_num"));
     }
 
+    // This sets up the initial state before the bug is triggered.
     function pre_inv() internal {
         vm.selectFork(forkId);
         preDaoBalance = address(targetAddr).balance;
     }
 
+    // If the bug doesn't work, it will remain the same as the pre-invocation balance.
+    // If the bug works, the balance will be drained.
+    // This is the post-invocation check.
     function post_inv() internal {
         uint256 postBalance = address(targetAddr).balance;
         assertEq(
@@ -34,12 +38,16 @@ contract DAOTest is Test, BugReport {
         );
     }
 
+    // The underlying tool relies on the bug implementing a test interface. 
+    // This is the test that will be run by the tool for a submitted bug report.
     function testConstraint() public {
         pre_inv();
         bug(forkId, targetAddr);
         post_inv();
     }
 
+    // Additional test to check if the total supply of the DAO is non-zero to
+    // begin with and remains so after the bug is triggered.
     function testTotalSupplyIsNonzero() public {
         bug(forkId, targetAddr);
         uint256 supply = DAO(targetAddr).totalSupply();
