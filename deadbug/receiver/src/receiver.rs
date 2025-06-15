5b50ead04a4bcc338783a3143c2df9a2dd65b7eb
Mariarosaria Barbaraci <mariarosaria.barbaraci@inf.unibe.ch>
2025-06-15 12:59:59 -0400

    addr added

--- deadbug/receiver/src/receiver.rs
+++ deadbug/receiver/src/receiver.rs
@@ -29,8 +29,55 @@ impl Receiver {
         let (sk, pk) = keygen(&pp);
 
         let bug_info = BugInfo {
-            addr: "<contract_address>".to_string(),
-            rules: "<invariants of the bug>".to_string(),
+            addr: "0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413".to_string(),
+            rules: r#"// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "../bug_report.sol";
+
+interface DAO {
+    function totalSupply() external view returns (uint256);
+}
+
+contract DAOTest is Test, BugReport {
+    string  rpcUrl;
+    address targetAddr;
+    uint256 forkId;
+    uint256 preDaoBalance;
+
+    function setUp() public {
+        rpcUrl = vm.envString("MAINNET_RPC_URL");
+        targetAddr = vm.envAddress("TARGET");
+        forkId = vm.createFork(rpcUrl, vm.envUint("block_num"));
+    }
+
+    function pre_inv() internal {
+        vm.selectFork(forkId);
+        preDaoBalance = address(targetAddr).balance;
+    }
+
+    function post_inv() internal {
+        uint256 postBalance = address(targetAddr).balance;
+        assertEq(
+            postBalance,
+            preDaoBalance,
+            "DAO balance was drained!"
+        );
+    }
+
+    function testConstraint() public {
+        pre_inv();
+        bug(forkId, targetAddr);
+        post_inv();
+    }
+
+    function testTotalSupplyIsNonzero() public {
+        bug(forkId, targetAddr);
+        uint256 supply = DAO(targetAddr).totalSupply();
+        assertGt(supply, 0);
+    }
+}"#.to_string(),
         };
 
         let enc_keys = EncKeys::key_gen();
